9b43ee3d577c460a541f2cb2ae73dac4c471bc55
Cody Wang <codywang@Codys-MacBook-Pro-2.local>
2022-02-09 10:44:16 -0500

    wip

--- shell.nix
+++ shell.nix
@@ -21,16 +21,13 @@ in mkShell {
 
   shellHook = ''
     export NIX_SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
+    export ETHERSCAN_API_KEY=8F2EN3ZZYKZKG1MV1J3APZVF24EE9W6129
     unset SSL_CERT_FILE
-  export PATH="/nix/store/a7i4hha4gh7fsbw7bfz51l2dkhgvb59a-curl-7.65.3-bin/bin/curl:$PATH"
-  export SETH_CHAIN=ethlive
-  # export ETH_FROM=0xf5808c41c895d88ca8103b5147b5f0e2c910d5a3
-  # export ETH_RPC_URL=https://eth-kovan.alchemyapi.io/v2/we7JjRdw2d-UvwuGVOSdkbQ28u8_On-0
-  # export ETH_KEYSTORE=./secrets
-  # export ETH_PASSWORD=./.pwd
-  export ETH_FROM=0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
-  export ETH_RPC_ACCOUNTS=yes
-  export ETH_RPC_URL=http://localhost:8546
+  
+    export ETH_FROM=0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
+    export ETH_RPC_URL=http://127.0.0.1:8545/
+    export SETH_CHAIN=rinkeby
+    export ETH_RPC_ACCOUNTS=yes
 
     setup-env() {
       . ${dds}/lib/setup-env.sh
